a1c2a363b54c5c21473a4cd93fe4ed203e701d44
donghwan <kdhe9797@naver.com>
2025-06-17 14:18:21 +0900

    기본 세팅

---
+++ src/main/java/com/nsmm/esg/csddd_service/CsdddServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsmm.esg.csddd_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdddServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdddServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsmm/esg/csddd_service/CsdddServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsmm.esg.csddd_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsdddServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
